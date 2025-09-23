ab1563ec05933e3adce9bc05c353f991996a2cc6
ngtiendat90 <you@example.com>
2025-09-23 15:13:12 +0700

    update

--- app/src/main/java/com/example/pdftool/di/Modules.kt
+++ app/src/main/java/com/example/pdftool/di/Modules.kt
@@ -1,10 +1,16 @@
 package com.example.pdftool.di
 
 import com.example.pdftool.domain.activities.MainActivity
+import com.example.pdftool.domain.fragment.HomeFragment
+import com.example.pdftool.domain.fragment.RecentFragment
+import com.example.pdftool.domain.fragment.BookmarksFragment
 import org.koin.dsl.module
 
 val mainActivity = module {
     scope<MainActivity> {
+        scoped { HomeFragment() }
+        scoped { RecentFragment() }
+        scoped { BookmarksFragment() }
     }
 }
 
